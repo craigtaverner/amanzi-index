e7c8236f0f10c531a478fc1409c26a7f4a9798b5
keithkid <keithkid.aptx4869@gmail.com>
2011-10-11 15:33:48 +0200

    Add IndexStats to the Index Relationship.

--- src/main/java/org/amanzi/index/AmanziIndexRelationshipTypes.java
+++ src/main/java/org/amanzi/index/AmanziIndexRelationshipTypes.java
@@ -3,5 +3,5 @@ package org.amanzi.index;
 import org.neo4j.graphdb.RelationshipType;
 
 public enum AmanziIndexRelationshipTypes implements RelationshipType {
-	AMANZI_INDEX, INDEX_CHILD, INDEX_NEXT, INDEX_CONFIG, INDEX_ROOT, INDEX_LEAF;
+	AMANZI_INDEX, INDEX_CHILD, INDEX_NEXT, INDEX_CONFIG, INDEX_ROOT, INDEX_STATS, INDEX_LEAF;
 }
